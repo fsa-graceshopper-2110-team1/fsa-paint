fa10ce16387266082c43ab72fc965954c0644236
Marina Chevis <marinachevis@gmail.com>
2022-02-11 09:47:59 -0500

    products API

--- server/api/products.js
+++ server/api/products.js
@@ -4,10 +4,35 @@ const {
 } = require("../db");
 module.exports = router;
 
+// GET /api/products
 router.get("/", async (req, res, next) => {
   try {
     const products = await Product.findAll({ order: ["id"] });
     res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/products/categories
+router.get("/categories", async (req, res, next) => {
+  try {
+    let categories = await Product.findAll({
+      order: ["category"],
+      attributes: ["category"],
+    });
+    categories = [...new Set(categories.map((cat) => cat.category))];
+    res.send(categories);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/products/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.json(product);
   } catch (err) {
     next(err);
   }
