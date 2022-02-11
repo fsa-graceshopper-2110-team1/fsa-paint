4b33a9da36963c83116e127b3b60e75ebe3a32ef
Marina Chevis <marinachevis@gmail.com>
2022-02-11 11:18:44 -0500

    finished order and orderitems routes

--- server/api/orderItems.js
+++ server/api/orderItems.js
@@ -4,11 +4,24 @@ const {
 } = require("../db");
 module.exports = router;
 
+// GET /api/orderItems/
 router.get("/", async (req, res, next) => {
   try {
     const orderItems = await OrderItem.findAll({
       order: ["id"],
-      include: ["orders"],
+    });
+    res.json(orderItems);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/orderItems/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const orderItems = await OrderItem.findAll({
+      where: { id: req.params.id },
+      order: ["id"],
     });
     res.json(orderItems);
   } catch (err) {
--- server/api/orders.js
+++ server/api/orders.js
@@ -42,3 +42,15 @@ router.post("/", async (req, res, next) => {
     next(err);
   }
 });
+
+// PUT /api/orders/:id {order}
+router.put("/:id", async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    //updating the entire user to be able to use this route to update any property
+    order.update(req.body);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
