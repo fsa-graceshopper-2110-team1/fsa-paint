38659315f1582368aa7455ca7b0a2d9b1830ec90
Marina Chevis <marinachevis@gmail.com>
2022-02-11 18:03:00 -0500

    added product reducer and dispatch in app

--- client/App.js
+++ client/App.js
@@ -1,26 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "./store";
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
-import Button from '@mui/material/Button'
-import { LoginModal } from './components/LoginModal'
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+import Button from "@mui/material/Button";
+import { LoginModal } from "./components/LoginModal";
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false)
-  const openModal = ()=>{
-    setShowModal(prev =>!prev)
-  }
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
   return (
     <div>
       <Navbar />
       <Button onClick={openModal}>Modal Toggle</Button>
-      <LoginModal showModal={showModal} setShowModal={setShowModal}/>
+      <LoginModal showModal={showModal} setShowModal={setShowModal} />
       <Routes />
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
