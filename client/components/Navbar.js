028a4a59c96d0e3e646a68bc734e40efc1f6cee8
Caitlin Morrison Paulos <49004574+caitlincampmorrison@users.noreply.github.com>
2022-02-13 11:55:59 -0800

    Update Navbar.js

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -269,7 +269,6 @@ const Navbar = () => {
         ) : (
           <div>
          <Link to="/login">Login</Link>
-            {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
