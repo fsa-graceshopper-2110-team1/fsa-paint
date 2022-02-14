16ff7464fdaf2680dc2f96d0d24a30bc7052284a
Marina Chevis <marinachevis@gmail.com>
2022-02-13 19:33:20 -0500

    started refactoring routes as prep for v6 - everything still works normally

--- client/Routes.js
+++ client/Routes.js
@@ -9,41 +9,37 @@ import Category from "./components/Category";
 import Product from "./components/Product";
 import { me } from "./store";
 import { CartPage } from "./components/CartPage";
+import RequireAuth from "./components/Auth/RequireAuth";
 
 /**
  * COMPONENT
  */
 const Routes = () => {
-  // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-  // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-  const user = useSelector((state) => state.auth);
-  const isLoggedIn = !!user.id;
+  // // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+  // // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+  // const user = useSelector((state) => state.auth);
+  // const isLoggedIn = !!user.id;
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(me());
-  }, [JSON.stringify(user)]);
+  // useEffect(() => {
+  //   dispatch(me());
+  // }, [JSON.stringify(user)]);
 
   return (
     <div>
-      {isLoggedIn ? (
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/shipping" component={ShippingForm} />
-          <Route path="/browse/:category" component={Category} />
-          <Route path="/browse" component={Browse} />
-          <Route path="/product/:productId" component={Product} />
-          <Route path="/cart" component={CartPage} />
-          <Redirect to="/home" />
-        </Switch>
-      ) : (
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </Switch>
-      )}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Home} />
+        <Route path="/signup" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/browse" component={Browse} />
+        <Route path="/browse/:category" component={Category} />
+        <Route path="/product/:productId" component={Product} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="/shipping" element={ShippingForm} />
+        <Redirect to="/home" />
+      </Switch>
     </div>
   );
 };
