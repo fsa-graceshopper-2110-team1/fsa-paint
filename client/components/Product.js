1d58e7df2b31101909de270976cb85f5ea4fd64a
Caitlin Morrison Paulos <49004574+caitlincampmorrison@users.noreply.github.com>
2022-02-19 08:49:40 -0800

    Update Product.js

--- client/components/Product.js
+++ client/components/Product.js
@@ -45,7 +45,7 @@ export const Product = () => {
       },
     },
   });
-  const[count,setCount] = useState(0)
+  const[count,setCount] = useState(1)
   return (
     <ThemeProvider theme={theme}>
       <div>
