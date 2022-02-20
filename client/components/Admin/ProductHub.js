efa7fd242b8efb0c1ffc3a654e59624d5516f088
Marina Chevis <marinachevis@gmail.com>
2022-02-20 13:38:34 -0500

    editable cells

--- client/components/Admin/ProductHub.js
+++ client/components/Admin/ProductHub.js
@@ -1,19 +1,70 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { updateProduct } from "../../store";
 
 const ProductHub = () => {
   const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  const [rows, setRows] = useState([]);
 
-  const GridRowsProp = products || [];
+  useEffect(() => {
+    setRows(products);
+  }, [products]);
+
+  //   const dispatchProductPromise = () => {
+  //     return useCallback(
+  //       (product) => new Promise((resolve) => resolve(product)),
+  //       []
+  //     );
+  //   };
+
+  const [snackbar, setSnackbar] = React.useState(null);
+  const handleCloseSnackbar = () => setSnackbar(null);
+
+  const handleCellEditCommit = async (params) => {
+    try {
+      // Dispatch product update
+      const response = await dispatch(
+        updateProduct({
+          id: params.id,
+          [params.field]: params.value,
+        })
+      );
+
+      setSnackbar({
+        children: "Product successfully saved",
+        severity: "success",
+      });
+      setRows((prev) =>
+        prev.map((row) =>
+          row.id === params.id ? { ...row, ...response } : row
+        )
+      );
+    } catch (error) {
+      console.log(error);
+      setSnackbar({
+        children: "Error while saving product",
+        severity: "error",
+      });
+      // Restore the row in case of error
+      setRows((prev) => [...prev]);
+    }
+  };
 
   const locked = ["id", "createdAt", "updatedAt"];
-  const GridColDef = Object.keys(GridRowsProp[0] || {}).map((key) => {
+  const GridColDef = Object.keys(rows[0] || {}).map((key) => {
     return {
       field: key,
       headerName: key.toUpperCase(),
       width: 150,
       editable: locked.indexOf(key) === -1 ? true : false,
+      preProcessEditCellProps: (params) => {
+        const isValid = !!params.props.value;
+        return { ...params.props, error: !isValid };
+      },
     };
   });
 
@@ -21,7 +72,17 @@ const ProductHub = () => {
     <div>
       <h3>Manage Products</h3>
       <div style={{ height: 600, width: "100%" }}>
-        <DataGrid rows={GridRowsProp} columns={GridColDef} />
+        <DataGrid
+          rows={rows}
+          columns={GridColDef}
+          experimentalFeatures={{ preventCommitWhileValidating: true }}
+          onCellEditCommit={handleCellEditCommit}
+        />
+        {!!snackbar && (
+          <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={6000}>
+            <Alert {...snackbar} onClose={handleCloseSnackbar} />
+          </Snackbar>
+        )}
       </div>
     </div>
   );
