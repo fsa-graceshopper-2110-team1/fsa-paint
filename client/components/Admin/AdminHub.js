c37073fea3bb480e310cc61809e1aa424f8dd37a
Marina Chevis <marinachevis@gmail.com>
2022-02-20 19:15:50 -0500

    added links to other admin hubs

--- client/components/Admin/AdminHub.js
+++ client/components/Admin/AdminHub.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ProductHub from "./ProductHub";
+import { Link } from "react-router-dom";
 
 const AdminHub = () => {
   return (
     <div>
       <h3>Admin Hub</h3>
-      <ProductHub />
+      <Link to="/product-hub">Manage Products</Link>
+      <Link to="/user-hub">View Users</Link>
+      <Link to="/order-hub">View Orders</Link>
     </div>
   );
 };
